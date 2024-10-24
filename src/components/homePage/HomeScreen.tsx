e6823434b0076858b1b8315abb184c61f551dbe1
Votha-Chheng <votha_chheng@gmx.fr>
2024-10-24 16:28:19 +0200

    Suppression du service à domicile

--- src/components/homePage/HomeScreen.tsx
+++ src/components/homePage/HomeScreen.tsx
@@ -7,6 +7,9 @@ import Approche from './Approche'
 import ServiceDomicile from './ServiceDomicile'
 import MassageIntuitif from './MassageIntuitif'
 import VideoHomePage from './VideoHomePage'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import { imgAppear } from '@/@animations/approcheAnimations'
 
 const HomeScreen: FC = () => {
 
@@ -26,6 +29,10 @@ const HomeScreen: FC = () => {
       <Approche/>
       <VideoHomePage/>
       <MassageIntuitif/>
+      <motion.div variants={imgAppear} initial="initial" animate="animate" className="relative min-w-[20%] hidden lg:block mb-44 justify-between self-center">
+        <Image src="/images/logoLotus.svg" alt='logo en forme de lotus' className='mx-auto' width={100} height={100}/>
+        <div className="w-24 h-1 mt-1 bg-white rounded-md mx-auto"/>
+      </motion.div>
     </main>     
   )
 }
