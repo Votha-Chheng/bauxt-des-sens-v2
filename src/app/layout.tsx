593e7407b26d61d082093770fef6839ae6481ab9
Votha-Chheng <votha_chheng@gmx.fr>
2025-10-04 11:06:39 +0200

    Implémentation de GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/sharedUI/Footer'
 import MenuHead from '@/components/sharedUI/MenuHead'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,6 +32,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTMID!} />
       <body className={inter.className} suppressHydrationWarning>
         <MenuHead/>
         {children}
